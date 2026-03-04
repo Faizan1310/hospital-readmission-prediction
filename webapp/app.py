4364154614c5fe445641d2d5d2427a125c1a2a24
Faizan Khan <faizan23khan0312@email.com>
2026-03-04 14:40:22 +0530

    Fixed indentation error in app.py

--- webapp/app.py
+++ webapp/app.py
@@ -243,6 +243,6 @@ def clear_history():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-   if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
     
